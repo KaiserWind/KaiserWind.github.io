cd8a114b08c9ca34cfc23ac162d657ba35c7c108
KaiserWind <kaiserwind13@gmail.com>
2023-09-21 16:33:10 +0800

    parallax mobile fix

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 let honkai =  document.getElementById('honkai');
-let male = document.getElementById('male');
-let female = document.getElementById('female');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY
@@ -8,10 +6,19 @@ window.addEventListener('scroll', () => {
         window.scrollY(0)
     } else{
     honkai.style.marginTop = value * 2.5  + 'px';
-    male.style.left = value * 1.5  + 'px';
-    female.style.left = value * -1.5  + 'px';
     }
 });
+
+if (window.matchMedia("(max-width: 700px)").matches) {
+    window.addEventListener('scroll', () => {
+        let value = window.scrollY
+        if(window.scrollY >= window.innerHeight){
+            window.scrollY(0)
+        } else{
+        honkai.style.marginTop = value * 0.5  + 'px';
+        }
+    });
+}
 
 document.getElementById("myaudio").volume = 0.6
 
